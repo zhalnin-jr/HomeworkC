0763a231de1b71d1f3f788638c35ff57107f1e9f
grigoriy <gz@gamil.com>
2023-04-17 16:30:39 +0300

    completed homework6

---
+++ HW6/Nomer41/Program.cs
@@ -0,0 +1,40 @@
+﻿int Enter(string numb)
+{
+    Console.Write(numb);
+    string num = Console.ReadLine();
+    int result = int.Parse(num);
+    return result;
+}
+int[] inputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Enter($"Введите ({i + 1}) элемент: ");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+int Count(int[ ] array) 
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.Clear();
+int length = Enter("Enter the size of massive: ");
+int[] array = inputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {Count(array)}");
---
+++ HW6/Nomer43/Program.cs
@@ -0,0 +1,25 @@
+﻿double Enter(string numb)
+{
+    Console.Write(numb);
+    string num = Console.ReadLine();
+    double result = double.Parse(num);
+    return result;
+}
+void Start()
+{
+    Console.Clear();
+    double b1 = Enter($"b1: ");
+    double k1 = Enter($"k1: ");
+    double b2 = Enter($"b2: ");
+    double k2 = Enter($"k2: ");
+    Console.WriteLine($"Координаты точки пересечения (x,y) - {searchPoint(b1,k1,b2,k2)}");
+}
+(double,double) searchPoint(double b1, double k1, double b2, double k2)
+{
+    double x=0;
+    double y=0;
+    x = (b2 - b1)/(k2 - k1);
+    y = (b1*k2-b2*k1)/(k2-k1);
+    return(x,y);
+}
+Start();
